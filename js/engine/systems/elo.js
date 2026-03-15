d0eee092e339450ea345ed31f26afe0dece21cd4
Linok-B <123248369+Linok-B@users.noreply.github.com>
2026-03-16 00:27:14 +0100

    Update elo.js

--- js/engine/systems/elo.js
+++ js/engine/systems/elo.js
@@ -1,1 +1,15 @@
+// Standard ELO Calculation Formula
+export function calculateElo(ratingA, ratingB, scoreA, kFactor = 32) {
+    // Expected scores
+    const expectedA = 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
+    const expectedB = 1 / (1 + Math.pow(10, (ratingA - ratingB) / 400));
 
+    // New Ratings
+    const newRatingA = Math.round(ratingA + kFactor * (scoreA - expectedA));
+    
+    // scoreB is the opposite of scoreA (if A wins (1), B loses (0))
+    const scoreB = 1 - scoreA; 
+    const newRatingB = Math.round(ratingB + kFactor * (scoreB - expectedB));
+
+    return { newRatingA, newRatingB };
+}
