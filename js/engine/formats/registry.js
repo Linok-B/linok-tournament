0940064e608072ed7eeac4ebac0433d96765e41d
Linok-B <123248369+Linok-B@users.noreply.github.com>
2026-03-17 05:48:29 +0100

    Update registry.js

--- js/engine/formats/registry.js
+++ js/engine/formats/registry.js
@@ -1,12 +1,11 @@
-// js/engine/formats/registry.js
 import * as SingleElimination from './elimination.js';
 import * as RoundRobin from './roundRobin.js';
-// import * as Swiss from './swiss.js'; // We will build this next
+import * as Swiss from './swiss.js'; // NEW
 
 export const Formats = {
     "single_elimination": SingleElimination,
     "round_robin": RoundRobin,
-    // "swiss": Swiss
+    "swiss": Swiss // NEW
 };
 
 export function getFormat(type) {
