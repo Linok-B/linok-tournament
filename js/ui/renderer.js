8de829bc57a707d367a082b39ea4cc8a915fa413
Linok-B <123248369+Linok-B@users.noreply.github.com>
2026-03-22 07:27:10 +0100

    Update renderer.js
    syntax fix again

--- js/ui/renderer.js
+++ js/ui/renderer.js
@@ -367,7 +367,7 @@ function drawBracketMath(stage, isActiveStage, tournament) {
             matchCoordinates[match.id] = { x: currentX + boxWidth, y: currentY };
             board.appendChild(createMatchBoxHTML(match, currentX, currentY, boxWidth, boxHeight, isActiveStage));
         });
-    });
+    };
 
     applyPanAndZoom(document.getElementById('bracket-viewport'), board);
 }
