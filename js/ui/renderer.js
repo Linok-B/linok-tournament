7f752773ecc20534b394f642dd962b375c487f61
Linok-B <123248369+Linok-B@users.noreply.github.com>
2026-03-29 07:44:33 +0200

    Update renderer.js

--- js/ui/renderer.js
+++ js/ui/renderer.js
@@ -491,7 +491,7 @@ function createMatchBoxHTML(match, x, y, width, height, isActiveStage, tournamen
     } else if (isActiveStage) {
         matchBox.innerHTML = `
             <div style="display:flex; height:100%; align-items:center; position: relative;">
-                <small style="position: absolute; top: -20px; right: 0; font-size: 10px;">${bracketLabel}</small>
+                <small style="position: absolute; top: -5px; right: 0; font-size: 10px;">${bracketLabel}</small>
                 <div style="overflow:hidden; padding-right:10px; width: 140px;">
                     <div title="${p1Name}" style="overflow:hidden; text-overflow:ellipsis; white-space:nowrap; margin-bottom:5px; height: 20px; line-height: 20px;">${p1Name}</div>
                     <div title="${p2Name}" style="overflow:hidden; text-overflow:ellipsis; white-space:nowrap; margin-bottom:5px; height: 20px; line-height: 20px;">${p2Name}</div>
