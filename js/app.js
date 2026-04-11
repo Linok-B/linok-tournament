89c35a4886fef0bbb0ddb756e35911e4a18f6fce
Linok-B <123248369+Linok-B@users.noreply.github.com>
2026-04-11 07:25:57 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -686,9 +686,9 @@ document.addEventListener('click', (e) => {
         
         // Change icon based on state
         if (document.body.classList.contains('streamer-mode')) {
-            e.target.innerHTML = getIcon('closedEye', 20);
-        } else {
             e.target.innerHTML = getIcon('openEye', 20);
+        } else {
+            e.target.innerHTML = getIcon('closedEye', 20);
         }
         // Ensure background always matches the panel
         e.target.style.background = "var(--bg-panel)";
