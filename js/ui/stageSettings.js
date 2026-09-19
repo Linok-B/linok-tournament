e53ec41b8569d437e079b1212433b7f0ee9a171b
Linok-B <123248369+Linok-B@users.noreply.github.com>
2026-09-19 23:00:34 +0200

    Update stageSettings.js
    huh... wtf.... I dont even have a storage.js file...
    I might be losing it

--- js/ui/stageSettings.js
+++ js/ui/stageSettings.js
@@ -1,7 +1,7 @@
 import { getIcon } from './icons.js';
 import { openSwissConfigModal } from './swissConfigModal.js';
 import { openStandingsColumnsModal } from './standingsColumns.js';
-import { saveTournamentLocally } from './storage.js';
+import { saveTournamentLocally } from '../storage/localData.js';
 
 export function openStageSettingsModal(stageIndex, tournament, onComplete) {
     const modal = document.getElementById('stage-settings-modal');
